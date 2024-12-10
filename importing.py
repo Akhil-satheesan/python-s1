6103f977f91801edfb6bd233c7b3f203b9cbb179
Akhil-satheesan <akhilsatheesan557@gmail.com>
2024-12-10 11:29:19 +0530

    importing code

---
+++ importing.py
@@ -0,0 +1,16 @@
+from graphics import circle,rectangle
+from graphics.dgraphics import cuboid,sphere
+r=int(input("Enter the radius of circle:"))
+circle.arecr(r)
+circle.percr(r)
+l=int(input("\nEnter the length of rectangle"))
+b=int(input("\nEnter the breadth of rectangle"))
+rectangle.arear(l,b)
+rectangle.perir(l,b)
+l1=int(input("Enter the  length of cuboid: "))
+b1=int(input("Enter the  Breadth  of cuboid: "))
+h1=int(input("Enter the  Height of cuboid: "))
+cuboid.arecub(l1,b1,h1)
+cuboid.pericub(l1,b1,h1)
+r1=int(input("Enter the  Radius of Sphere: "))
+
